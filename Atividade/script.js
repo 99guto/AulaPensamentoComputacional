4415071b7ca5c4834b55a44c955499fcc581ea83
Augusto Mendes <161895489+99guto@users.noreply.github.com>
2024-03-01 22:06:48 -0300

    Add files via upload

---
+++ Atividade/script.js
@@ -0,0 +1,16 @@
+document.getElementById('form').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    var nome = document.getElementById('name').value 
+    var nota1 = parseInt(document.getElementById('nota1').value)
+    var nota2 = parseInt(document.getElementById('nota1').value)
+    var nota3 = parseInt(document.getElementById('nota1').value)
+    
+    var media = Match.round((nota1+nota2+nota3)/ 3)
+    
+    var resultado = document.getElementById('resultado')
+    var mensagem = document.getElementById('mensagem')
+
+    
+
+})
